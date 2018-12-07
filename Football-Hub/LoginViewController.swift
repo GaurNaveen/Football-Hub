670deb7e67f8594217c5fd105ac2304b66246bdb
Naveen Gaur <naveengaur800@gmail.com>
2018-12-07 22:57:52 +0530

    Added tap gesture recognizer on the text fields.

--- Football-Hub/LoginViewController.swift
+++ Football-Hub/LoginViewController.swift
@@ -9,8 +9,17 @@
 import UIKit
 import GoogleSignIn
 class LoginViewController: UIViewController,GIDSignInUIDelegate,UITextFieldDelegate {
+    // Labels on the left of Email and Password text fields.
+    @IBOutlet weak var ELabel: UILabel!
+    @IBOutlet weak var PLabel: UILabel!
+    
     @IBOutlet weak var userEmail: UITextField!
     @IBOutlet weak var password: UITextField!
+    var tapGesture = UITapGestureRecognizer()
+    var tapGestureForPassword = UITapGestureRecognizer()
+    @IBAction func signIn(_ sender: Any) {
+        print(userEmail.text ?? "none")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,10 +32,34 @@ class LoginViewController: UIViewController,GIDSignInUIDelegate,UITextFieldDeleg
         // Set the delegate for text fields for user inputs.
         userEmail.delegate = self
         password.delegate = self
+        
+        // Add Gesture Recogniser to both Text Fields.
+        hideLabelsOnTextFields()
+        userEmail.addGestureRecognizer(tapGesture)
+        password.addGestureRecognizer(tapGestureForPassword)
     }
    
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         
+    }
+    
+    
+    func hideLabelsOnTextFields(){
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(textFieldTapped(_:)))
+        tapGestureForPassword = UITapGestureRecognizer(target: self, action: #selector(passwordTextFieldTapped(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        tapGestureForPassword.numberOfTapsRequired = 1
+    }
+    
+    @objc func textFieldTapped(_ recognizer: UITapGestureRecognizer ) {
+        // Make the labels hide.
+        ELabel.isHidden = true
+        PLabel.isHidden = false
+    }
+    
+    @objc func passwordTextFieldTapped(_ recognizer: UITapGestureRecognizer){
+        PLabel.isHidden = true
+        ELabel.isHidden = false
     }
 }
 
