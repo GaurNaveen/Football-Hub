972eef943800dd634e1f756b418cae55d74e58ee
Naveen Gaur <naveengaur800@gmail.com>
2018-12-07 21:55:53 +0530

    Changed the UI for the Login Page.

--- Football-Hub/LoginViewController.swift
+++ Football-Hub/LoginViewController.swift
@@ -8,29 +8,32 @@
 
 import UIKit
 import GoogleSignIn
-class LoginViewController: UIViewController,GIDSignInUIDelegate {
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        
-    }
+class LoginViewController: UIViewController,GIDSignInUIDelegate,UITextFieldDelegate {
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var password: UITextField!
     
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         GIDSignIn.sharedInstance().uiDelegate = self
-
-        view.backgroundColor = UIColor.init(netHex: 0x160E78)
         let signInButton = GIDSignInButton(frame: CGRect(x: 121, y: 534, width: 150, height: 43))
         view.addSubview(signInButton)
+        
+        // Set the delegate for text fields for user inputs.
+        userEmail.delegate = self
+        password.delegate = self
     }
    
-    
-    
-    
-    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+    }
+}
 
+// MARK: - Sets up the text fields.
+extension LoginViewController {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
